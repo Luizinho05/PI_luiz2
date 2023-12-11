a2663532e4d71ee57a354f7ed0a783e239e9c65a
luiz <luizhsimionato@gmail.com>
2023-12-11 11:33:21 -0300

    a

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -5,9 +5,9 @@ export default function Header() {
     return (
         <div class="container-xs">
             <nav class="navbar navbar-dark bg-dark">
-                <a class="navbar-brand" href="/"><img src={LOGO} alt="LOGO" style={{width: 50, height: 60}}/></a>
-                <a class="navbar-brand" href="/">Vista-se! | Vestuário e identidade</a>
                 <a class="navbar-brand" href="/Login">Login</a>
+                <a class="navbar-brand" href="/">Vista-se! | Vestuário e identidade</a>
+                 <a class="navbar-brand" href="/"><img src={LOGO} alt="LOGO" style={{width: 50, height: 60}}/></a>
             </nav>
         </div>
     )
